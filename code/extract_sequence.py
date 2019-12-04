d15617bb10c3e09e3d9b9ee897b6773d33f74738
liwenran <26700222+liwenran@users.noreply.github.com>
2019-12-04 22:50:36 +0800

    Add files via upload

---
+++ code/extract_sequence.py
@@ -0,0 +1,35 @@
+import sys
+import numpy as np
+
+def extract_sequence():
+    num_donor = 635
+    region_list = [[] for i in range(num_donor)]
+    fasta_sequence_dict = SeqIO.to_dict(SeqIO.parse(open('hg19.fa'), 'fasta'))
+    file = open('variants.txt')
+    count = 0
+    for line in file:
+        line = line.strip().split('\t')
+        count += 1
+        ref_seq = str(fasta_sequence_dict[line[0]].seq[int(line[1]):int(line[2])])
+        for i in range(num_donor):
+            if line[8+i]=='0/0':
+                continue
+            if line[8+i]=='1/1' or line[8+i]=='1/0' or line[8+i]=='0/1':
+                var_pos = int(line[4])-int(line[1])
+                var_seq = ref_seq[:var_pos]+line[7]+ref_seq[var_pos+len(line[6]):]
+            elif line[8+i]=='./.':
+                var_pos = int(line[4])-int(line[1])
+                var_seq = ref_seq[:var_pos]+ref_seq[var_pos+len(line[6]):]
+            else:
+                print line[8+i]
+            region_list[i].append('>'+'_'.join(line[:6])+'\n'+var_seq)
+    file.close()
+
+    for i in range(num_donor):
+        fout = open('temp/var_seqs.donor'+str(i)+'.fa', 'w')
+        fout.write('\n'.join(region_list[i]))
+        fout.close()
+    return
+##
+extract_sequence()
+
---
+++ code/generate_binding_matrix.py
@@ -0,0 +1,82 @@
+import sys
+import numpy as np
+
+def IDs():
+	ID_list = []
+	file = open('temp/var_seqs.donor'+str(i))
+	for line in file:
+		line = line.strip().split('\t')
+		ID_list.append('_'.join(line[:3]))
+	file.close()
+	return ID_list
+
+def motifs():
+	motif_dict = {}
+	file = open('all_motif_rmdup')
+	fout = open('motifs_col.txt', 'w')
+	ind = 0
+	for line in file:
+		if '>'!=line[0]:
+			continue
+		line = line.split('\t')
+		fout.write(line[1]+'\n')
+		if line[1] not in motif_dict:
+			motif_dict[line[1]] = ind
+			ind += 1
+	fout.close()
+	file.close()
+	return motif_dict
+
+def TFs():
+	motif_dict = motifs()
+	TF_dict = {}
+	file = open('motif-TF.txt')
+	for line in file:
+		line = line.strip().split('\t')
+		if line[0] in motif_dict:
+			if line[1] not in TF_dict:
+				TF_dict[line[1]] = line[0]
+			else:
+				TF_dict[line[1]] += ' '+line[0]
+	file.close()
+
+	fout = open('TFs_col.txt', 'w')
+	for tf in TF_dict:
+		fout.write(tf+'\t'+TF_dict[tf]+'\n')
+	fout.close()
+	return TF_dict
+
+def bind_dict():
+	ID_list = IDs()
+	TF_dict = TFs()
+	RE_dict_score = {}
+	RE_dict_count = {}
+	file = open('find-out.txt')
+	for line in file:
+		line = line.strip().split('\t')
+		if len(line)!=6 or line[0]=='PositionID':
+			continue
+		if (line[0],line[3]) not in RE_dict_score:
+			RE_dict_score[(line[0],line[3])] = float(line[-1])
+			RE_dict_count[(line[0],line[3])] = 1
+		else:
+			RE_dict_score[(line[0],line[3])] += float(line[-1])
+			RE_dict_count[(line[0],line[3])] += 1
+	file.close()
+
+	fout = open('RE_binding_count','w')
+	for ID in ID_list:
+		for tf in TF_dict:
+			c = 0
+			for motif in TF_dict[tf].split(' '):
+				if (ID, motif) in RE_dict_score:
+					c += RE_dict_count[(ID, motif)]
+			fout.write(str(c)+'\t')
+		fout.write('\n')
+	fout.close()
+	return
+
+##MAIN
+bind_dict()
+
+
