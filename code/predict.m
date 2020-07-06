1db9cd7f688b49cf42e4e89a80303f29c308c0e4
liwenran <26700222+liwenran@users.noreply.github.com>
2020-07-06 20:19:19 +0800

    Predict var-level open scores

--- code/predict.m
+++ code/predict.m
@@ -42,19 +42,40 @@ end
 save('params.mat', 'alpha','-v7.3');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% GTEx data
-Ens_expr=dlmread('GTEx_rnaseq.txt','\t',0,1);
-Ens_expr=log2(Ens_expr+1);
-
+%% predict openness score for each donor
+%% Download from GTEx project
 fid = fopen('GTEx_rnaseq.txt');
 C = textscan(fid, '%s %*[^\n]');
 fclose(fid);
-Ens_genes=C{1,1};
+Genes=C{1,1};
 
-[tfind,index]=ismember(TFs,Ens_genes);
-GTEx_TFexpr=Ens_expr(index(index>0),:);
+%%
+fid = fopen('TFs_col.txt');
+C = textscan(fid, '%s %*[^\n]');
+fclose(fid);
+TFs=C{1,1};
 
-%%predict GTEx
-%load('params_TFexpr.mat')
-GTEx_Open=alpha(1,1:N)'+full(((alpha(2:end,1:N).*TF_binding)'*GTEx_TFexpr)')';
-save('pred_GTEx_open.mat', 'GTEx_Open','-v7.3');
+%%
+[tfind,index1]=ismember(TFs,Genes);
+
+%%var-level TFBS based on GTEx WGS 
+TF_binding=dlmread('var_binding_count.txt','\t',0,0);
+TF_binding=single(TF_binding(:,tfind)');
+
+%%RE-level params
+load('params.mat');
+alpha=single(alpha);
+
+%%var-level params
+index=load('index_from_RE_to_var.tmp');
+params=alpha(:,index);
+
+%% GTEx expr data
+% load TFexpr
+GTEx_TFexpr=dlmread('TFexpr.txt','\t',0,1); % exprs of TFs for GTEx samples
+GTEx_TFexpr=log2(GTEx_TFexpr+1);
+
+%%var-level open scores
+pred_var=params(1,:)'+full(((params(2:end,:).*TF_binding)'*GTEx_TFexpr)')';
+save('pred_var_open_score.mat', 'pred_var','-v7.3');
+
