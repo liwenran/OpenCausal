e403fd0e1e9c0f877efe16a3306cd9b983bd66ff
liwenran <26700222+liwenran@users.noreply.github.com>
2020-07-13 21:58:41 +0800

    Predict var-level open scores

--- code/predict.m
+++ code/predict.m
@@ -1,47 +1,6 @@
 pool=parpool('local',6,'IdleTimeout', 8000)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%loading training data (Download from ENCODE project)
-expr=dlmread('ENCODE_expr.txt','\t',0,1);
-expr=log2(expr+1);
-
-fid = fopen('ENCODE_expr.txt');
-C = textscan(fid, '%s %*[^\n]');
-fclose(fid);
-Genes=C{1,1};
-
-%%
-fid = fopen('TFs_col.txt');
-C = textscan(fid, '%s %*[^\n]');
-fclose(fid);
-TFs=C{1,1};
-
-[tfind,index]=ismember(TFs,Genes);
-TFexpr=expr(index(index>0),);
-
-%%
-TF_binding=dlmread('RE_binding_count.txt','\t',0,0);
-TF_binding=TF_binding(:,tfind)';
-%TF_binding(TF_binding>0)=1;
-
-%%
-Open=dlmread('ENCODE_open.bed','\t',0,3);
-Open=log2(1+Open);
-
-%%
-N=size(Open,1);
-
-%training
-parfor i=1:N
-    y=Open(i,:)';
-    X=full((TF_binding(:,i).*TFexpr)');
-    [beta FitInfo]=lasso(X,y,'CV',5,'Alpha',0.5);
-    alpha(:,i)=[FitInfo.Intercept(FitInfo.IndexMinMSE);beta(:,FitInfo.IndexMinMSE)];
-end
-
-save('params.mat', 'alpha','-v7.3');
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% predict openness score for each donor
 %% Download from GTEx project
 fid = fopen('GTEx_rnaseq.txt');
@@ -58,10 +17,6 @@ TFs=C{1,1};
 %%
 [tfind,index1]=ismember(TFs,Genes);
 
-%%var-level TFBS based on GTEx WGS 
-TF_binding=dlmread('var_binding_count.txt','\t',0,0);
-TF_binding=single(TF_binding(:,tfind)');
-
 %%RE-level params
 load('params.mat');
 alpha=single(alpha);
@@ -75,7 +30,21 @@ params=alpha(:,index);
 GTEx_TFexpr=dlmread('TFexpr.txt','\t',0,1); % exprs of TFs for GTEx samples
 GTEx_TFexpr=log2(GTEx_TFexpr+1);
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%var-level TFBS based on GTEx WGS 
+TF_binding=dlmread('var_binding_count.txt','\t',0,0);
+TF_binding=single(TF_binding(:,tfind)');
+
 %%var-level open scores
 pred_var=params(1,:)'+full(((params(2:end,:).*TF_binding)'*GTEx_TFexpr)')';
 save('pred_var_open_score.mat', 'pred_var','-v7.3');
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%var-level TFBS based on reference genome
+TF_binding=dlmread('ref_binding_count.txt','\t',0,0);
+TF_binding=single(TF_binding(:,tfind)');
+
+%%var-level open scores
+pred_ref=params(1,:)'+full(((params(2:end,:).*TF_binding)'*GTEx_TFexpr)')';
+save('pred_ref_open_score.mat', 'pred_ref','-v7.3');
